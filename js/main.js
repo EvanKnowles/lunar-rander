43e33d26197d925c8fb81f49618a3bd5af8dd589
Evan Knowles <evanjamesknowles@gmail.com>
2017-05-24 07:25:58 +0200

    Pulling three months of data and displaying - need a server cache.
    Date roll is broken.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     const DEATH_Y = 20;
     const DEATH_X = 10;
     const DEATH_ROT = 0.174533;
-    const TIME_SPAN = 7;
+    const TIME_SPAN = 90;
 
     // DEATH ROT DEATH ROT! To the tune of Dethklok, of course.
 
@@ -31,7 +31,12 @@
                 days[index] = out.rates.ZAR;
                 console.log('Checkout this exchange: ', out.rates.ZAR);
             })
-            .catch(err => console.error(err));
+            .catch(err => {
+                if (err.indexOf("429")) {
+                    console.log(err);
+                    console.log('SLOW DOWN!');
+                }
+            });
     }
 
     let days = [];
@@ -46,9 +51,12 @@
         }
         return count;
     };
+
     async function loadRates() {
         for (var i = 0; i < TIME_SPAN; i++) {
             fetchRate(date, i);
+            await sleep(200);
+            console.log(i/TIME_SPAN);
             date = unroll(date);
         }
 
@@ -56,6 +64,7 @@
            await sleep(100);
         }
         console.log(days);
+        loadShip();
 
     }
 
@@ -128,12 +137,14 @@
 
     let lastTime, thisTime;
 
+    const TIME_WIDTH = canvas.width / TIME_SPAN;
     function floorHeight(x) {
-        return Math.sin(x / 10) * 2 + FLOOR;
+        let day = Math.floor(x / TIME_WIDTH);
+        return days[day]*20 + FLOOR;
     }
 
     var floorRough = function (x) {
-        return (Math.abs(floorHeight(x - 5) - floorHeight(x + 5)) > 5);
+        return Math.abs(floorHeight(x - 5) - floorHeight(x + 5)) > 5;
     };
 
     function loop() {
@@ -169,7 +180,7 @@
         showAt("Fuel: " + Math.round(ship.fuel), 24, 10, 36);
         showAt("Velocity: " + ship.vel() + " m/s", 24, 10, 72);
 
-        if (ship.pos.y > FLOOR - shipImage.height / 2) {
+        if (ship.pos.y > floorHeight(ship.pos.x) - shipImage.height / 2) {
 
             // falling too fast, drifting too hard or off by ~10 degrees.
             if (Math.abs(ship.velocity.y) >= DEATH_Y || Math.abs(ship.velocity.x) >= DEATH_X || Math.abs(ship.rotate) >= DEATH_ROT) {
@@ -197,5 +208,7 @@
 
         loop();
     };
-    shipImage.src = "img/ship.png";
+    function loadShip() {
+        shipImage.src = "img/ship.png";
+    }
 })();
